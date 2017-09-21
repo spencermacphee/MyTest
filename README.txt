e05c844fcfb4a105fbd6d2684e000da616b08d60
spencermacphee <spencermacphee@eastlink.ca>
2017-09-21 16:46:51 -0300

    FirstAttempt

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is my first attempt at a local git repository.
---
+++ temperatureConverter.py
@@ -0,0 +1,3 @@
+fahrenheit = int(input('Temperature in Fahrenheit:  '))
+celsius = (fahrenheit - 32) *5/9
+print(celsius, ' degrees Celsius')
